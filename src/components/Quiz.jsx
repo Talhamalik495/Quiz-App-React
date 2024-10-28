9c1185863cd8312a03ad18514afb10e8e7a3e7ab
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-10-28 19:08:13 +0500

    Update Ui

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,13 +6,13 @@ function Quiz() {
   console.log(data);
 
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center gap-7 bg-[rgb(0,0,41)] text-white">
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-10 bg-[rgb(0,0,41)] text-white">
       <div className="w-full flex justify-center">
-        <h1 className="text-3xl">Quiz App</h1>
+        <h1 className="text-4xl">Quiz App</h1>
       </div>
-      <div className="min-h-72 flex  px-3 flex-col w-[500px] bg-white text-black rounded-md  py-5  gap-4">
+      <div className="min-h-80 flex  px-3 flex-col w-[500px] bg-white text-black rounded-md  py-5  gap-7">
         <div>
-          <h1>{data[0].q}</h1>
+          <h1 className="text-3xl">{data[0].q}</h1>
         </div>
         <div className="flex gap-2">
           <input type="radio" />
@@ -29,6 +29,11 @@ function Quiz() {
         <div className="flex gap-2">
           <input type="radio" />
           <p>{data[0].d}</p>
+        </div>
+        <div className="flex gap-2">
+          <button className="px-10 h-10 bg-[rgb(0,0,41)] text-white rounded-md">
+            Next
+          </button>
         </div>
       </div>
     </div>
